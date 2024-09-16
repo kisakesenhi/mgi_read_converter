6a53d741ac245f2701674db6de6b2e9458469032
fas <fas@bioLinuxFrance>
2024-09-16 08:38:21 +0200

    Fixed errors

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ use clap::{arg, command, value_parser, ArgAction, Command};
 use regex::Regex;
 
 // Capacity 
-// const CAPACITY: usize = 10240; // will be used on flate2::GzDecoder,commented now!
+const CAPACITY: usize = 10240; // will be used on flate2::GzDecoder,commented now!
 fn check_inputfiles(inputfilename:&PathBuf)->Result<PathBuf,io::Error>{
     //check if path exists
     if ! inputfilename.exists() { return(Err(std::io::Error::new(io::ErrorKind::NotFound,"File not found")))  }
@@ -44,7 +44,7 @@ fn convert_fastq(inputfilename:&PathBuf , outputfilename:&PathBuf ) ->Result<(),
     // Input values:
     //
     // While using gzip decoder from flate2 
-    let in_fh = std::fs::File::open(in_filename).unwrap();
+    let in_fh = std::fs::File::open(inputfilename).unwrap();
     let in_gz = MultiGzDecoder::new(in_fh);
     let in_buf = io::BufReader::with_capacity(CAPACITY, in_gz);
     /*
@@ -74,7 +74,7 @@ fn convert_fastq(inputfilename:&PathBuf , outputfilename:&PathBuf ) ->Result<(),
         let header:&str = str::from_utf8(&c_record.head).unwrap();
         match mgi_readheader2_illuminaheader(header){
             Ok(new_header) =>{
-                            c_record.head=newheader.as_bytes().to_vec();
+                            c_record.head=new_header.as_bytes().to_vec();
                             match c_record.write(&mut out_buf){
                                     Ok(_) => true,// if writes success fully continue parsing
                                     _ => false // if write not successfull  stop parsing 
@@ -143,7 +143,7 @@ fn mgi_readheader2_illuminaheader(inputstring: &str) -> Result<String, String> {
         c = c.trim_start_matches('0').to_string();
         r = r.trim_start_matches('0').to_string();
         tile = tile.trim_start_matches('0').to_string();
-        let output=format!("M00001:1:{}:{}:{}:{}:{} 1:N:0:1", fc, l, tile, c, r,pair);    Ok(output)
+        let output=format!("M00001:1:{}:{}:{}:{}:{} {}:N:0:1", fc, l, tile, c, r,pair);    Ok(output)
     }else{
         Err(format!("Read name: {}\n\t format does not match to pattern '([A-Z]\\d+)L(\\d)C(\\d\\d\\d)R(\\d\\d\\d)(\\d+)\\/(\\d)$'",inputstring))
     }
